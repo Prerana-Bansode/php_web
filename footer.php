7c74eea8b87a802688f2da5513d6c5646996190a
Prerana <preranabansoderiyality@gmail.com>
2024-06-04 11:18:39 +0530

    php web

---
+++ footer.php
@@ -0,0 +1,121 @@
+
+   <footer class="cs_footer cs_primary_bg cs_ternary_color">
+      <div class="cs_footer_main">
+        <div class="container">
+          <div class="cs_footer_grid_4">
+            <div class="cs_footer_grid_item">
+              <div class="cs_footer_item">
+                <div class="cs_text_widget">
+                  <img src="assets/img/footer-logo.svg" alt="Logo">
+                  <p>Welcome to Seapearl, where coastal luxury meets unparallel  hospitality.</p>
+                </div>
+              </div>
+            </div>
+            <div class="cs_footer_grid_item">
+              <div class="cs_footer_item">
+                <h2 class="cs_widget_title cs_fs_28 cs_white_color">Contact Us</h2>
+                <ul class="cs_menu_widget cs_mp0">
+                  <li>
+                    <p class="cs_mb_22">452 15h Street, Office 741, Ohio, De 47754, USA</p>
+                  </li>
+                  <li class="cs_white_color">Call : <a href="tel:+00(244)14-50-774">+00(244)14-50-774</a></li>
+                  <li class="cs_white_color">Email : <a href="mailto:info@oceanpearl.com">info@oceanpearl.com</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="cs_footer_grid_item">
+              <div class="cs_footer_item">
+                <h2 class="cs_widget_title cs_fs_28 cs_white_color">Links</h2>
+                <ul class="cs_menu_widget cs_mp0">
+                  <li><a href='index.html'>Home</a></li>
+                  <li><a href='about-dark.html'>About Us</a></li>
+                  <li><a href='rooms-dark.html'>Booking</a></li>
+                  <li><a href='spa-wellness-dark.html'>SPA & Wellness</a></li>
+                  <li><a href='restaurant-dark.html'>Restaurant</a></li>
+                  <li><a href='blog-dark.html'>News & Media</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="cs_footer_grid_item">
+              <div class="cs_footer_item">
+                <h2 class="cs_widget_title cs_fs_28 cs_white_color">Subscribe</h2>
+                <div class="cs_newsletter cs_style_1 cs_color_1">
+                  <form action="#" class="cs_newsletter_form position-relative">
+                    <input type="email" class="cs_newsletter_input cs_radius_5" placeholder="Enter Your Email Address">
+                    <button class="cs_btn cs_style_2 cs_medium cs_radius_5 cs_fs_15">
+                      Subscribe
+                      <span>
+                        <i>
+                          <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M9.00431 0.872828C9.00431 0.458614 8.66852 0.122828 8.25431 0.122828L1.50431 0.122827C1.0901 0.122827 0.754309 0.458614 0.754309 0.872828C0.754309 1.28704 1.0901 1.62283 1.50431 1.62283H7.50431V7.62283C7.50431 8.03704 7.84009 8.37283 8.25431 8.37283C8.66852 8.37283 9.00431 8.03704 9.00431 7.62283L9.00431 0.872828ZM1.53033 8.65747L8.78464 1.40316L7.72398 0.342497L0.46967 7.59681L1.53033 8.65747Z" fill="currentColor"></path>
+                          </svg>                                        
+                        </i>
+                        <i>
+                          <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M9.00431 0.872828C9.00431 0.458614 8.66852 0.122828 8.25431 0.122828L1.50431 0.122827C1.0901 0.122827 0.754309 0.458614 0.754309 0.872828C0.754309 1.28704 1.0901 1.62283 1.50431 1.62283H7.50431V7.62283C7.50431 8.03704 7.84009 8.37283 8.25431 8.37283C8.66852 8.37283 9.00431 8.03704 9.00431 7.62283L9.00431 0.872828ZM1.53033 8.65747L8.78464 1.40316L7.72398 0.342497L0.46967 7.59681L1.53033 8.65747Z" fill="currentColor"></path>
+                          </svg>                   
+                        </i>
+                      </span>
+                    </button>
+                  </form>
+                  <p class="cs_newsletter_subtitle cs_ternary_color cs_mb_30">Stay In the Loop & unlock exclusive Experiences. Subscribe for Exclusive Updates and Offers!</p>
+                  <div class="cs_social_btns cs_style_1">
+                    <a href="#" class="cs_center">
+                      <i class="fa-brands fa-linkedin-in"></i>
+                    </a>
+                    <a href="#" class="cs_center">
+                      <i class="fa-brands fa-twitter"></i>
+                    </a>
+                    <a href="#" class="cs_center">
+                      <i class="fa-brands fa-youtube"></i>
+                    </a>
+                    <a href="#" class="cs_center">
+                      <i class="fa-brands fa-facebook-f"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="cs_bottom_footer_wrap">
+        <div class="container">
+          <div class="cs_bottom_footer position-relative">
+            <div class="cs_copyright">OceanPearl Copyright ©  <span class="cs_copyright_year">2024</span> <a href="#">Laralink</a></div>
+            <span class="cs_scrollup cs_type_1 cs_center">              
+              <svg width="15" height="7" viewBox="0 0 15 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M15 6.18793L14.1169 7L7.93687 1.31723C7.81958 1.20941 7.66053 1.14885 7.49468 1.14885C7.32884 1.14885 7.16978 1.20941 7.0525 1.31723L0.884376 6.99022L0 6.177L6.16812 0.505163C6.51998 0.181708 6.99715 0 7.49468 0C7.99222 0 8.46938 0.181708 8.82125 0.505163L15 6.18793Z" fill="white"/>
+              </svg>  
+              <span class="cs_scrollup_bg_dotted cs_accent_color">
+                <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="28" cy="28" r="27.5" stroke="currentColor" stroke-dasharray="5 5"/>
+                </svg>  
+              </span>              
+            </span>
+            <div class="cs_bottom_footer_right">
+              <ul class="cs_footer_links cs_mp_0">
+                <li>
+                  <a href="#">Privacy & Cookie Policy</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- End Footer -->
+
+    <!-- Script -->
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/jquery.slick.min.js"></script>
+    <script src="assets/js/swiper.min.js"></script>
+    <script src="assets/js/moment.min.js"></script>
+    <script src="assets/js/daterangepicker.min.js"></script>
+    <script src="assets/js/lightgallery.min.js"></script>
+    <script src="assets/js/YTPlayer.min.js"></script>
+    <script src="assets/js/main.js"></script>
+  </body>
+</html>
+
---
+++ header.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+  
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Laralink">
+    <!-- Favicon Icon -->
+    <link rel="icon" href="assets/img/favicon.png">
+    <title>SaePearl - Resort & Hotel HTML Template</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/slick.min.css">
+    <link rel="stylesheet" href="assets/css/swiper.min.css">
+    <link rel="stylesheet" href="assets/css/daterangepicker.css">
+    <link rel="stylesheet" href="assets/css/lightgallery.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body class="cs_dark">
+    <div class="cs_preloader cs_accent_color cs_primary_font">
+      <div class="cs_preloader_in"></div>
+    </div>
+    <header class="cs_site_header cs_style_1 cs_primary_color cs_sticky_header">
+      <div class="cs_main_header">
+        <div class="container-fluid">
+          <div class="cs_main_header_in">
+            <div class="cs_main_header_left">
+              <a class='cs_site_branding' href='index.html'>
+                <img src="assets/img/logo.svg" alt="Logo">
+              </a>
+            </div>
+            <div class="cs_main_header_center">
+              <div class="cs_nav cs_fs_13 cs_semibold">
+                <ul class="cs_nav_list">
+                  <li class="menu-item-has-children cs_mega_menu">
+                    <a href='index.html'>Home</a>
+                    <ul class="cs_mega_wrapper cs_with_thumb">
+                      <li>
+                        <a href='index.html'>
+                          <img src="assets/img/demo/home-1.jpg" alt="">Default Hotel & Resort
+                        </a>
+                      </li>
+                      <li>
+                        <a href='home-v2.html'>
+                          <img src="assets/img/demo/home-2.jpg" alt="">Hotel & Resort V2
+                        </a>
+                      </li>
+                      <li>
+                        <a href='home-v3.html'>
+                          <img src="assets/img/demo/home-3.jpg" alt="">Hotel & Resort V3
+                        </a>
+                      </li>
+                      <li>
+                        <a href='home-v4.html'>
+                          <img src="assets/img/demo/home-4.jpg" alt="">Hotel & Resort V4
+                        </a>
+                      </li>
+                      <li>
+                        <a href='home-v5.html'>
+                          <img src="assets/img/demo/home-5.jpg" alt="">Hotel & Resort V5
+                        </a>
+                      </li>
+                      <li>
+                        <a href='home-v6.html'>
+                          <img src="assets/img/demo/home-6.jpg" alt="">Hotel & Resort V6
+                        </a>
+                      </li>
+                      <li>
+                        <a href='home-v7.html'>
+                          <img src="assets/img/demo/home-7.jpg" alt="">Hotel & Resort V7
+                        </a>
+                      </li>
+                      <li>
+                        <a href='home-v8.html'>
+                          <img src="assets/img/demo/home-8.jpg" alt="">Hotel & Resort V8
+                        </a>
+                      </li>
+                      <li>
+                        <a href='home-v9.html'>
+                          <img src="assets/img/demo/home-9.jpg" alt="">Hostel
+                        </a>
+                         </li>
+                      <li>
+                        <a href='home-v10.html'>
+                          <img src="assets/img/demo/home-10.jpg" alt="">Luxury Hotel
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li><a href='about-dark.html'>About Us</a></li>
+                  <li class="menu-item-has-children">
+                    <a href='rooms-dark.html'>Rooms</a>
+                    <ul>
+                      <li><a href='rooms-dark.html'>Room Grid View</a></li>
+                      <li><a href='rooms-list-view-dark.html'>Room List View</a></li>
+                      <li><a href='room-details-dark.html'>Room Details V1</a></li>
+                      <li><a href='room-details-v2-dark.html'>Room Details V2</a></li>
+                      <li><a href='room-details-v3-dark.html'>Room Details V3</a></li>
+                      <li><a href='search-result-dark.html'>Room Search Result</a></li>
+                      <li><a href='complete-reservation-dark.html'>Payment Page</a></li>
+                    </ul>
+                  </li>
+                  <li><a href='services-dark.html'>Services</a></li>
+                  <li><a href='restaurant-dark.html'>Restaurant</a></li>
+                  <li class="menu-item-has-children">
+                    <a href="#">Pages</a>
+                    <ul>
+                      <li><a href='restaurant-dark.html'>Restaurant & Bar</a></li>
+                      <li><a href='spa-wellness-dark.html'>SPA & Wellness</a></li>
+                      <li><a href='infinity-pool-dark.html'>Infinity Pool</a></li>
+                      <li><a href='team-dark.html'>Team</a></li>
+                      <li><a href='gallery-dark.html'>Gallery</a></li>
+                      <li><a href='faq-dark.html'>FAQ</a></li>
+                      <li><a href='404-dark.html'>Error 404</a></li>
+                    </ul>
+                  </li>
+                  <li class="menu-item-has-children">
+                    <a href='blog-dark.html'>Blog</a>
+                    <ul>
+                      <li><a href='blog-dark.html'>Sidebar Blog</a></li>
+                      <li><a href='blog-grid-dark.html'>Blog Grid View</a></li>
+                      <li><a href='blog-list-dark.html'>Blog List View</a></li>
+                      <li><a href='blog-details-dark.html'>Blog Details</a></li>
+                    </ul>
+                  </li>
+                  <li><a href='contact-dark.html'>Contact</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="cs_main_header_right">
+              <a class='cs_btn cs_style_2 cs_medium cs_radius_20 cs_fs_15' href='rooms-dark.html'>
+                Reservation
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
